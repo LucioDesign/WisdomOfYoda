b0ac356ee12435f5353ba9dc10254cf6339f7073
Lucio Jaurena <66427557+LucioDesign@users.noreply.github.com>
2021-08-31 11:29:49 -0300

    Final code
    - changed the functions to use async/await
    - cleaner code
    - made errors log on console and change the "advice" text

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-'use strict'
-console.log('holis');
-
+"use strict";
+console.log("hello there");
 
 const YODA_API_URL = "https://api.funtranslations.com/translate/yoda.json";
 const CUOTE_API_URL = "https://api.adviceslip.com/advice";
 
-const btn = document.querySelector('#btn');
-const heroText = document.querySelector('#hero-title');
+const btn = document.getElementById("btn");
+const heroText = document.getElementById("hero-title");
 let advice;
 
-btn.onclick = frase;
+  // calling for translation
+const getURL = (text) => `"${YODA_API_URL}?text=${text}"`;
 
-function getURL(text) {                   // API URL builder function
-    return YODA_API_URL + "?" + "text=" + text;
-  }
+  // Error handling function
+const errorHandler = (error) => {
+  console.error(error);
+  heroText.innerHTML = "Yoda is tired, try again later";
+  heroText.style.fontSize = "2.5rem";
 
- function frase(YodaUrl){
-    fetch(CUOTE_API_URL).then((response)=>response.json()).then((tip)=>{
-        advice = tip.slip.advice
-//        heroText.innerHTML = `"${advice}"`;
-    function errorHandler(error) {
-        alert("Yoda is tired, try again later (API call limit exceeded).");  // Error handling function 
-         }
-    function translate(){
-        fetch(getURL(advice))
-        .then((response) => response.json())
-        .then(json => {
-          let translatedtext = json.contents.translated;
-          advice = translatedtext;
-          console.log(advice);
-          heroText.innerHTML = `"${advice}"`;
-        })
-        .catch(errorHandler);
-    }
-    translate();
-    heroText.style.fontSize = "2.5rem";
-    btn.innerHTML = `ask again`;
+  btn.innerHTML = `ask again`;
+};
+  // calling advice phrase
+const getAdvice = async () => {
+  const response = await fetch(CUOTE_API_URL);
+
+  const tip = await response.json();
+
+  return tip.slip.advice;
+};
+
+  // translating advice
+async function yodaTranslate() {
+  try {
+    advice = await getAdvice();
+
+    const response = await fetch(getURL(advice));
+
+    const json = await response.json();
+
+    advice = json.contents.translated;
     console.log(advice);
-})
+
+    heroText.innerHTML = `"${advice}"`;
+    heroText.style.fontSize = "2.5rem";
+
+    btn.innerHTML = `ask again`;
+  } catch (error) {
+    errorHandler(error);
+  }
 }
 
-
-
-
-
-
+btn.onclick = yodaTranslate;
