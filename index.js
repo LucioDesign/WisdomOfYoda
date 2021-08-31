06ef99afa339a9f277653f74c0d89fdf6891d0a0
Lucio Jaurena <66427557+LucioDesign@users.noreply.github.com>
2021-08-30 23:59:58 -0300

    JS implementation
    - both APIs merged, 
    - yoda's API is succesful but needs to be fixed at the DOM.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 'use strict'
+console.log('holis');
+
+
+const YODA_API_URL = "https://api.funtranslations.com/translate/yoda.json";
+const CUOTE_API_URL = "https://api.adviceslip.com/advice";
+
+const btn = document.querySelector('#btn');
+const heroText = document.querySelector('#hero-title');
+let advice;
+
+btn.onclick = frase;
+
+function getURL(text) {                   // API URL builder function
+    return YODA_API_URL + "?" + "text=" + text;
+  }
+//const advice;
+ function frase(YodaUrl){
+    fetch(CUOTE_API_URL).then((response)=>response.json()).then((tip)=>{
+        advice = tip.slip.advice
+    function errorHandler(error) {
+        alert("Yoda is tired, try again later (API call limit exceeded).");  // Error handling function 
+         }
+    function translate(){
+        fetch(getURL(advice))
+        .then((response) => response.json())
+        .then(json => {
+          let translatedtext = json.contents.translated;
+          advice = translatedtext;
+          console.log(advice);
+          return advice = translatedtext;
+        })
+        .catch(errorHandler);
+    }
+//   translate();
+    //document.getElementById('hero-title').id = 'hero-title';
+    heroText.style.fontSize = "2.5rem";
+    heroText.innerHTML = `"${advice}"`;
+    btn.innerHTML = `ask again`;
+    console.log(advice);
+})
+}
+
+
+
+
+
+
